542b121df2f4b5215aa0cd2235b2a29018142bd7
Anusara12012004 <66010914119@msu.ac.th>
2026-01-27 12:19:53 +0700

    a

--- g/connect.php
+++ g/connect.php
@@ -1,7 +1,7 @@
 <?php
 			$host = "localhost";
 				$user = "root";
-				$pwd = "";
+				$pwd = "ff2004";
 				$db = "4119db";
 				$conn = mysqli_connect($host ,$user, $pwd, $db) or die ("เชื่อมต่อข้อมูลไม่ได้");
 				mysqli_query($conn, "SET NAMES utf8");
